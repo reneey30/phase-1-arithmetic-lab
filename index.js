ea0a52f0aa670e76122a88fc11073f82163a429a
Irene Njeru <irenenjeru8@gmail.com>
2022-04-06 20:14:03 +0300

    completed task

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 11);
+
+let num3 = 4;
+let num4 = 16;
+let mod = num3 % num4;
+
+let myArray = [4,5,2,6,14,20];
+let max = Math.max(...myArray);
